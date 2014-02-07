ae6714aa21c0f3f3245b84db5e5d5427d3cffc00
Jonathan Sisk <jonsisk@gmail.com>
2014-02-07 09:29:01 -0700

    About Us Sidebar and Team Page
    Added custom sidebar for About Us Page
    Created post types for Staff and Board
    Added content to Our Team page
    Added post order plugin

--- www.eatonsenior.dev/wp-content/themes/eatonsenior/functions.php
+++ www.eatonsenior.dev/wp-content/themes/eatonsenior/functions.php
@@ -1,6 +1,16 @@
 <?php
 /* ---- Additional Widget Areas ----- */
 register_sidebar( array(
+	'name' => __( 'About Us Sidebar'),
+	'id' => 'about_us_sidebar',
+	'description' => __( 'Sidebar on About Us page'),
+	'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+	'after_widget' => '</aside>',
+	'before_title' => '<h3 class="widget-title">',
+	'after_title' => '</h3>',
+	) );
+	
+register_sidebar( array(
 	'name' => __( 'Footer Left Widget Area'),
 	'id' => 'footer-left',
 	'description' => __( 'Left widget area on footer'),
@@ -29,6 +39,42 @@ register_sidebar( array(
 	'before_title' => '<h3 class="widget-title">',
 	'after_title' => '</h3>',
 	) );
-	
+
+/* ---- Additional Menu ---- */
 register_nav_menu( 'footer', __( 'Footer Menu') );
+
+/* ---- Custom Post Types ---- */
+add_action( 'init', 'create_post_type' );
+function create_post_type() {
+	register_post_type( 'staff',
+		array(
+			'labels' => array(
+				'name' => __( 'Staff' ),
+				'singular_name' => __( 'Staff' ),
+				'add_new_item' => ('Add New Staff'),
+				'edit_item' => ('Edit Staff'),
+				'new_item' => ('New Staff'),
+				'view_staff' => ('View Staff')
+			),
+		'public' => true,
+		'has_archive' => false,
+		'supports' => array( 'title', 'editor', 'thumbnail')
+		)
+	);
+	register_post_type( 'board',
+		array(
+			'labels' => array(
+				'name' => __( 'Board' ),
+				'singular_name' => __( 'Board' ),
+				'add_new_item' => ('Add New Board'),
+				'edit_item' => ('Edit Board'),
+				'new_item' => ('New Board'),
+				'view_staff' => ('View Board')
+			),
+		'public' => true,
+		'has_archive' => false,
+		'supports' => array( 'title', 'editor', 'thumbnail')
+		)
+	);
+}
 ?>
---
+++ www.eatonsenior.dev/wp-content/themes/eatonsenior/sidebar-aboutus.php
@@ -0,0 +1,3 @@
+<div id="secondary" class="widget-area aboutus" role="complementary">
+	<?php dynamic_sidebar( 'about_us_sidebar' ); ?>
+</div>
